c84040d4f2aa69274af3a3a8a60d9871f0eba717
Paul Kuntke <paul.kuntke@tu-dresden.de>
2026-07-17 12:37:18 +0100

    Update

--- label2npyspace.py
+++ label2npyspace.py
@@ -8,9 +8,9 @@ import nibabel as nib
 import numpy as np
 import torch
 import torchio as tio
-
 from rich.logging import RichHandler
-from rich.progress import BarColumn, Progness, TextColumn, TimeRemainingColumn, track
+
+# from rich.progress import BarColumn, Progness, TextColumn, TimeRemainingColumn, track
 
 FORMAT = "%(message)s"
 logging.basicConfig(
@@ -19,38 +19,46 @@ logging.basicConfig(
 
 log = logging.getLogger("rich")
 
-def resize_image(input_file, output_file, size=96, crop=180, affine=None)
 
+def resize_image(input_file, output_file, size=96, crop=180, affine=None):
+    log.debug(f"resizing {input_file} to size {size} and cropping {crop}")
     transforms = tio.Compose(
         [
-            tio.Resample((1,1,1))
-            tio.CropOrPad((crop, crop, crop))
+            tio.Resample((1, 1, 1)),
+            tio.CropOrPad((crop, crop, crop)),
             tio.Resize(
                 (size, size, size),
-                label_interpolation='nearest',
-                image_interpolation='nearest'
-            )
+                label_interpolation="nearest",
+                image_interpolation="nearest",
+            ),
         ]
     )
-    subject = tio.Subject(img=tio.LabelMap(input_file))
-    subject = transforms(subject)
-    data = subject.img.data.squeeze(0).numpy()
+    image = tio.LabelMap(input_file)
+    transformed_image = transforms(image)
+    transformed_image.save(output_file)
     if affine is None:
         affine = np.eye(4)
 
-    nib_img = nib.Nifti1Image(data, affine=affine)
-    nib.save(nib_img, output_file)
+    # nib_img = nib.Nifti1Image(data, affine=affine)
+    # nib.save(nib_img, output_file)
+
 
 def main():
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Resize and crop images')
-    parser.add_argument('inputfile', type=str, required=True, help='input nifti file')
-    parser.add_argument('outputfile', type=str, required=True, help='output filename')
-    parser.add_argument('--size', type=int, default=96, help='size of outputimage (isotropic)')
-    parser.add_argument('--crop', type=int, default=180, help='cropping size, applied before resizing')
+    parser = argparse.ArgumentParser(description="Resize and crop images")
+    parser.add_argument("inputfile", type=str, help="input nifti file")
+    parser.add_argument("outputfile", type=str, help="output filename")
+    parser.add_argument(
+        "--size", type=int, default=96, help="size of outputimage (isotropic)"
+    )
+    parser.add_argument(
+        "--crop", type=int, default=180, help="cropping size, applied before resizing"
+    )
 
     args = parser.parse_args()
 
     resize_image(args.inputfile, args.outputfile, size=args.size, crop=args.crop)
+
+
+if __name__ == "__main__":
+    main()
---
+++ reg_fs2npy.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+
+#
+#
+# Convert the results from freesurfer into the same space as the cropped npy images to get valueable region ids for the heatmaps with
+#
+
+FS_SUBJECTS_DIR="${HOME}/freesurfer/subjects"
+OUTPUT_DIR="/mnt/bulk-mars/paulkuntke/RadBrainDL_msp/fs_results/"
+
+mkdir -p $OUTPUT_DIR
+
+subjects_file="../top20_worst_progressor_2ycutoff_pst_2z_flair.csv"
+
+
+
+
+while IFS= read -r subject_id; do
+  echo "Processing subject: $subject_id"
+
+  # Create subjects outputdir
+  mkdir -p "${OUTPUT_DIR}/sub-${subject_id}"
+
+  for labelfile in "aparc.a2009s+aseg" "aseg.auto" "aseg.presurf" "brainmask.auto" "T1" "T2" "wm.asegedit" "wm" "wmparc"
+  do
+      ./mri_convert "${FS_SUBJECTS_DIR}/${subject_id}/mri/${labelfile}.mgz"  "${OUTPUT_DIR}/sub-${subject_id}/sub-${subject_id}_${labelfile}.nii.gz"
+
+      python label2npyspace.py "${OUTPUT_DIR}/sub-${subject_id}/sub-${subject_id}_${labelfile}.nii.gz" "${OUTPUT_DIR}/sub-${subject_id}/sub-${subject_id}_space-numpy_${labelfile}.nii.gz"
+
+
+  done
+
+done < "$subjects_file"
+
+
+# ./mri_convert
